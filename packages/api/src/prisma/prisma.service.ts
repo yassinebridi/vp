9ed8df44e983ce92b6ff83dc884af5f704c07a92
Yassine Bridi <ybridi@gmail.com>
2021-04-17 18:19:23 +0000

    Setup prisma logging

--- packages/api/src/prisma/prisma.service.ts
+++ packages/api/src/prisma/prisma.service.ts
@@ -1,11 +1,31 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger();
+  constructor() {
+    super({ log: ['warn', 'error', 'query'] });
+  }
+
   async onModuleInit() {
+    this.$use(async (params, next) => {
+      const before = Date.now();
+      const result = await next(params);
+      const after = Date.now();
+      this.logger.log(
+        `Query took ${after - before}ms`,
+        `${params.model}.${params.action}`,
+      );
+      return result;
+    });
     await this.$connect();
   }
 
