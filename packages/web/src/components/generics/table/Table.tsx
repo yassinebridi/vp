4dfc21c7e1bd699226e8a65965d233d76ae77d54
Yassine Bridi <ybridi@gmail.com>
2021-04-23 19:04:23 +0000

    Open update drawer onClick the item from the table

--- packages/web/src/components/generics/table/Table.tsx
+++ packages/web/src/components/generics/table/Table.tsx
@@ -1,3 +1,5 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { UpdateItem } from "@components";
 import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/outline";
 import { useTableContext } from "@utils";
 import clsx from "clsx";
@@ -15,6 +17,9 @@ const Table: React.FC<TableProps> = ({
   columnsArray,
   hiddenColumnsArray,
 }) => {
+  const { isOpen: isOpen, onOpen, onClose } = useDisclosure();
+  const [id, setId] = React.useState<string>();
+
   const {
     setColumns,
     setTableState,
@@ -72,72 +77,81 @@ const Table: React.FC<TableProps> = ({
     setTableActions({ toggleAllRowsSelected });
   }, [setColumns, setTableState, state, selectedFlatRows]);
   return (
-    <div className="flex flex-col">
-      <div className="">
-        <table {...getTableProps()} className="min-w-full mt-3">
-          <thead className="">
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()} className="">
-                {headerGroup.headers.map((column, i) => (
-                  <th
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                    style={{ width: column.width }}
-                    className={clsx(
-                      "bg-[#f7f7f7] dark:bg-[#29282e] px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-400",
-                      i === 0 && "rounded-l-lg",
-                      i === headerGroup.headers.length - 1 && "rounded-r-lg"
-                    )}
-                  >
-                    <div className="flex flex-row cursor-pointer space-x-1">
-                      <span>{column.render("Header")}</span>
-                      <span className="">
-                        {column.isSorted ? (
-                          column.isSortedDesc ? (
-                            <ArrowDownIcon
-                              className="w-4 h-4"
-                              aria-label="sorted descending"
-                            />
-                          ) : (
-                            <ArrowUpIcon
-                              className="w-4 h-4"
-                              aria-label="sorted ascending"
-                            />
-                          )
-                        ) : null}
-                      </span>
-                    </div>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr
-                  {...row.getRowProps()}
-                  className="cursor-pointer hover:bg-[#f7f7f7] active:bg-gray-100 dark:active:bg-gray-800 dark:hover:bg-gray-850"
-                >
-                  {row.cells.map((cell, i) => (
-                    <td
-                      {...cell.getCellProps()}
+    <>
+      <div className="flex flex-col">
+        <div className="">
+          <table {...getTableProps()} className="min-w-full mt-3">
+            <thead className="">
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()} className="">
+                  {headerGroup.headers.map((column, i) => (
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      style={{ width: column.width }}
                       className={clsx(
-                        "whitespace-nowrap px-6 py-2",
+                        "bg-[#f7f7f7] dark:bg-[#29282e] px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-400",
                         i === 0 && "rounded-l-lg",
-                        i === row.cells.length - 1 && "rounded-r-lg"
+                        i === headerGroup.headers.length - 1 && "rounded-r-lg"
                       )}
                     >
-                      {cell.render("Cell")}
-                    </td>
+                      <div className="flex flex-row cursor-pointer space-x-1">
+                        <span>{column.render("Header")}</span>
+                        <span className="">
+                          {column.isSorted ? (
+                            column.isSortedDesc ? (
+                              <ArrowDownIcon
+                                className="w-4 h-4"
+                                aria-label="sorted descending"
+                              />
+                            ) : (
+                              <ArrowUpIcon
+                                className="w-4 h-4"
+                                aria-label="sorted ascending"
+                              />
+                            )
+                          ) : null}
+                        </span>
+                      </div>
+                    </th>
                   ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                console.log("row: ", row);
+                return (
+                  <tr
+                    {...row.getRowProps()}
+                    className="cursor-pointer hover:bg-[#f7f7f7] active:bg-gray-100 dark:active:bg-gray-800 dark:hover:bg-gray-850"
+                    onClick={() => {
+                      setId(row.original.id);
+                      onOpen();
+                    }}
+                  >
+                    {row.cells.map((cell, i) => (
+                      <td
+                        {...cell.getCellProps()}
+                        className={clsx(
+                          "whitespace-nowrap px-6 py-2",
+                          i === 0 && "rounded-l-lg",
+                          i === row.cells.length - 1 && "rounded-r-lg"
+                        )}
+                      >
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+
+      <UpdateItem isOpen={isOpen} onClose={onClose} id={id} />
+    </>
   );
 };
 
